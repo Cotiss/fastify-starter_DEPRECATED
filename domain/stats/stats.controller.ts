568b2c135b8c59e521c9d9fa2447a5c9bd2b25e5
matt01671 <matt01671@gmail.com>
2022-05-25 14:00:12 +1200

    use histogram

--- domain/stats/stats.controller.ts
+++ domain/stats/stats.controller.ts
@@ -2,7 +2,6 @@ import { FastifyInstance } from "fastify";
 
 export default async function listings(instance: FastifyInstance) {
   instance.get("/", async (request, reply) => {
-    const afterDate = new Date();
-    reply.send(instance.stats({ afterDate }));
+    reply.send(instance.stats());
   });
 }
---
+++ plugins/stats.ts
@@ -0,0 +1,73 @@
+// https://github.com/fastify/fastify-routes-stats/blob/master/index.js
+import {
+  PerformanceObserver,
+  performance,
+  RecordableHistogram,
+  createHistogram,
+} from "node:perf_hooks";
+import fp from "fastify-plugin";
+import { FastifyPluginAsync } from "fastify";
+
+const ONSEND = "on-send-";
+const ONREQUEST = "on-request-";
+const ROUTES = "fastify-routes:";
+
+export type RetrieveStats = () => void;
+
+const performanceMonitorPlugin: FastifyPluginAsync = async function (instance) {
+  let observedEntries: Record<string, Record<string, RecordableHistogram>> = {};
+  const obs = new PerformanceObserver((items) => {
+    items.getEntries().forEach((item) => {
+      if (item.name.indexOf(ROUTES) === 0) {
+        const method = item.name.split(":")[1].split("|")[0];
+        const route = item.name.split(":")[1].split("|")[1];
+        if (observedEntries[method] && observedEntries[method][route]) {
+          observedEntries[method][route].record(
+            Math.floor(item.duration * 1000000000)
+          );
+        } else {
+          if (!observedEntries[method]) {
+            observedEntries[method] = {};
+          }
+          observedEntries[method][route] = createHistogram();
+          observedEntries[method][route].record(
+            Math.floor(item.duration * 1000000000)
+          );
+        }
+      }
+    });
+    performance.clearMarks();
+  });
+  obs.observe({ entryTypes: ["measure"] });
+
+  instance.addHook("onRequest", function (request, reply, next) {
+    const id = request.id;
+    performance.mark(ONREQUEST + id);
+    next();
+  });
+
+  instance.addHook("onSend", function (request, reply, _, next) {
+    let routeId = request.raw.url;
+
+    const id = request.id;
+    performance.mark(ONSEND + id);
+
+    const key = `${ROUTES}${request.raw.method}|${routeId}`;
+    performance.measure(key, ONREQUEST + id, ONSEND + id);
+
+    performance.clearMarks(ONSEND + id);
+    performance.clearMarks(ONREQUEST + id);
+
+    next();
+  });
+
+  const stats: RetrieveStats = function () {
+    return observedEntries;
+  };
+
+  instance.decorate("stats", stats);
+};
+
+export default fp(performanceMonitorPlugin, {
+  name: "performance-monitor",
+});
